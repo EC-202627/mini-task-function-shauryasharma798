f9a5d96e56d4522553afcab2c7f591a25947d637
shauryasharma798 <shaurya.sharma@avantika.edu.in>
2026-03-28 17:50:26 +0530

    Update input method for calculating fines
    Refactor input handling to read book title and days overdue separately.

--- Problem_01.py
+++ Problem_01.py
@@ -1,16 +1,12 @@
 def calculate_fine(book_title, days_overdue, daily_rate=5.0, max_fine=150.0):
-    
     fine = days_overdue * daily_rate
-    
     if fine > max_fine:
         fine = max_fine
-    
     return fine
 
-data = input().rsplit(" ", 1)
 
-book_title = data[0]
-days_overdue = int(data[1])
+book_title = input()
+days_overdue = int(input())
 
 fine = calculate_fine(book_title, days_overdue)
 
